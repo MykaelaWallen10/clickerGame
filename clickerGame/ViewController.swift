b65aa53978deb40b1be33a419f0bfb1b5954b8ac
Mykaela Wallen <mykaelawallen@gmail.com>
2023-10-03 09:27:44 -0500

    Initial Commit

---
+++ clickerGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clickerGame
+//
+//  Created by MYKAELA WALLEN on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
